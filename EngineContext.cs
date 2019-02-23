e502c5de80c1a4c9f98ee5a19873d53b839e6295
dineshpatel86 <dinesh.p@shaligraminfotech.com>
2019-02-23 15:01:43 +0530

    Add files via upload

---
+++ EngineContext.cs
@@ -0,0 +1,25 @@
+﻿using Autofac.Integration.Mvc;
+using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace SampleAPIProject.Common
+{
+    public static class EngineContext
+    {
+        public static T Resolve<T>()
+        {
+            return AutofacDependencyResolver.Current.RequestLifetimeScope.Resolve<T>();
+            //return DependencyResolver.Current.GetService<T>();
+
+
+
+                
+
+
+          }
+    }
+}
+        
